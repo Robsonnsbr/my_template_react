b823a85d2221889681264f54939a6d8a07a16287
robsonnsbr <robsonnsbr@gmail.com>
2023-08-15 15:54:58 -0300

    feat: in development stage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,7 +3,7 @@ import css from "./Home.module.css";
 export const Home = () => {
   return (
     <div className={css.home}>
-      <h1>HOME</h1>TESTE<h3></h3>
+      <h1>HOME</h1>in development stage<h3></h3>
     </div>
   );
 };
