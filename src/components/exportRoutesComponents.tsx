c0e7de49d45cf7d2ac0aa176bc79194e45723d55
robsonnsbr <robsonnsbr@gmail.com>
2023-08-22 17:43:22 -0300

    fix: layout

--- src/components/exportRoutesComponents.tsx
+++ src/components/exportRoutesComponents.tsx
@@ -6,8 +6,7 @@ import { Slider } from "./commons/layout/slider/Slider";
 import { User } from "./commons/user/User";
 import { Search } from "./commons/user/Search";
 import { Error } from "./commons/user/Error";
-import { Loading } from "./commons/layout/loading/loading";
 
-export const routesComponents = { Main, Navbar, Footer, Slider, Loading };
+export const routesComponents = { Main, Navbar, Footer, Slider };
 
 export const routesComponentsUser = { User, Search, Error };
