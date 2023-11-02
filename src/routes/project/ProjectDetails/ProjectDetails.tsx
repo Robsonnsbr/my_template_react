e7d8ce12128739222b388a68e764fed7844026c2
robsonnsbr <robsonnsbr@gmail.com>
2023-11-02 01:25:26 -0300

    fit: fit layouts

--- src/routes/project/ProjectDetails/ProjectDetails.tsx
+++ src/routes/project/ProjectDetails/ProjectDetails.tsx
@@ -38,71 +38,63 @@ export const ProjectDetails = () => {
   // const projectName = location.state.projectName;
   // console.log(projectName);
   return (
-    <div className={css.containerProject}>
-      <div className={css.ProjectDetails}>
-        Exibindo informações do projeto: {`${name} id ${id}`}
-        {project && <div>{project.full_name}</div>}
-        <Link className="link" to={"/portfolio/projects"}>
-          <button>
-            <TbArrowBackUp />
-          </button>
-        </Link>
-        <Iframe
-          url="https://atacadao-app.vercel.app/Login"
-          width="750px"
-          height="100%"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-      </div>
-      <div className={css.information} style={{ width: "100%" }}>
-        <h3 style={{ margin: "20px", textAlign: "center" }}>
-          INFORMAÇÕES DO PROJETO:
-        </h3>
-        <div style={{ margin: "20px", textAlign: "center" }}>
-          <h3>Compatibilidade:</h3>
-          <p style={{ color: "#fff", margin: "20px", textAlign: "justify" }}>
-            O projeto Collectors Control não possui uma boa compatibilidade e
-            responsividade com dispositivos móveis, uma vez que não foi
-            desenvolvido com essa finalidade em mente. A intenção do sistema é
-            ser utilizado exclusivamente em dispositivos desktop, o que pode
-            limitar sua acessibilidade e usabilidade em smartphones e tablets.
-            Claro que tendo em vista tal necessidade, é possível melhorar a
-            experiência dos usuários que desejam acessar o sistema em
-            dispositivos móveis. Havendo essa possibilidade, podem ser feitas as
-            devidas alterações em uma versão posterior para incluir essas
-            plataformas na responsividade, ajudando ainda mais o projeto a se
-            tornar mais versátil e acessível a um público mais amplo.
-          </p>
-          <h3>
-            O Propósito e Benefícios do Sistema de Controle de Coletores no
-            Atacadão
+    <>
+      <Link className="link" to={"/portfolio/projects"}>
+        <button title="Voltar">
+          <TbArrowBackUp />
+        </button>
+      </Link>
+      <div className={css.containerProject}>
+        <section className={css.ProjectDetails}>
+          <Iframe
+            url="https://atacadao-app.vercel.app/Login"
+            width="750px"
+            height="100%"
+            id=""
+            className=""
+            display="block"
+            position="relative"
+          />
+        </section>
+        <section className={css.information} style={{ width: "100%" }}>
+          <h3 style={{ margin: "20px", textAlign: "center" }}>
+            Exibindo informações do projeto: {`${name} id ${id}`}
+            {project && <div>{project.full_name}</div>}
           </h3>
-          <p style={{ color: "#fff", margin: "20px", textAlign: "justify" }}>
-            Agora vamos explorar com mais detalhes a intenção por trás do
-            projeto. O sistema nasceu da necessidade específica de aprimorar o
-            controle dos equipamentos usados pela empresa Atacadão, uma divisão
-            do grupo Carrefour. A base dessa necessidade é o controle de uma
-            série de dispositivos conhecidos como coletores. A missão do sistema
-            é simplificar a gestão desses equipamentos, fornecendo informações
-            sobre sua disponibilidade para uso e identificando quem está
-            responsável por cada dispositivo a qualquer momento. Isso se torna
-            essencial devido ao grande número de equipamentos em circulação e
-            colaboradores, bem como a alta rotatividade na utilização, que
-            ocorre com diferentes períodos e dias de uso. O sistema foi
-            concebido para proporcionar um controle mais eficaz e uma alocação
-            mais eficiente dos coletores, assegurando que eles estejam sempre
-            prontos para uso quando necessário. Além disso, atribui clareza às
-            responsabilidades, otimizando os processos operacionais da empresa e
-            garantindo uma alocação mais eficiente dos recursos disponíveis.
-          </p>
-          <Link to="https://atacadao-app.vercel.app/Login" target="_blank">
-            Link do projeto na Vercel
-          </Link>
-        </div>
+          <div style={{ margin: "20px", textAlign: "center" }}>
+            <h3>Tecnologias</h3>
+            <p style={{ color: "#fff", margin: "20px", textAlign: "justify" }}>
+              O projeto utiliza React, React Router DOM, Swiper, Framer Motion,
+              e EmailJS para criar uma interface dinâmica e interativa. Além
+              disso, a API é desenvolvida com Node.js, Express, Mongoose e
+              Bcrypt para garantir autenticação e segurança de dados, com
+              TypeScript adicionando qualidade ao código
+            </p>
+            <h3>Compatibilidade:</h3>
+            <p style={{ color: "#fff", margin: "20px", textAlign: "justify" }}>
+              O Control Collectors &#40;AtacadãoApp&#41; não foi projetado para
+              mobile, é destinado apenas para uso em desktops. Melhorias para
+              torná-lo acessível em smartphones e tablets podem ser consideradas
+              em futuras versões, visando aprimorar a experiência para um
+              público mais amplo.
+            </p>
+            <h3>Propósito</h3>
+            <p style={{ color: "#fff", margin: "20px", textAlign: "justify" }}>
+              O sistema Control Collectors nasceu da necessidade de aprimorar o
+              controle dos coletores usados pela empresa Atacadão, uma divisão
+              do grupo Carrefour. Ele simplifica a gestão desses equipamentos,
+              fornecendo informações sobre disponibilidade e responsabilidade.
+              Isso é crucial devido ao grande número de dispositivos e
+              colaboradores, bem como à alta rotatividade na utilização. O
+              sistema assegura um controle eficaz, otimizando os processos
+              operacionais da empresa e a alocação de recursos
+            </p>
+            <Link to="https://atacadao-app.vercel.app/Login" target="_blank">
+              Link do projeto na Vercel
+            </Link>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   );
 };
